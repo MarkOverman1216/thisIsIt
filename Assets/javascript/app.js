f74a1353ca3e917554968a06684a4282f06ef19a
Chris Glenn <chrisglenn519@yahoo.com>
2019-10-19 13:14:17 -0500

    made ticketmaster interactive with genius

--- Assets/javascript/app.js
+++ Assets/javascript/app.js
@@ -1,5 +1,8 @@
 //makes apis calls when new song button is pressed
 $('#newSong').on('click',function(){
+    $('.tourEvent').text('');
+    $('.tourDate').text('');
+    $('.tourSale').text('');
     callGenius();
-    // getEvents();
 })
+
