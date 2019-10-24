5e3c4ef09e046aa894906f613b91c71d6043d4b1
Christopher Glenn <Chrisglenn519@yahoo.com>
2019-10-24 10:33:13 -0500

    bunch of stuff

--- Assets/javascript/apis/itunes.js
+++ Assets/javascript/apis/itunes.js
@@ -1,33 +1,68 @@
+
 function getSampleAudio(audio, artist) {
   var song = encodeURIComponent(audio)
-  var found = false
-  console.log(song)
+  console.log(audio + " / " + artist);
+  var audio1 = editName(audio);
+  var artist1 = editName(artist);
+  console.log(audio1 + " / " + artist1);
+  var playingAudio;
   $.ajax({
     'method': "GET",
-    'url': `https://itunes.apple.com/search?term=${song}&media=music`,
+    'url': `https://itunes.apple.com/search?term=${song}&media=music&limit=10`,
     'async': true,
     success: function (json) {
-      console.log(json)
-      json.results.forEach(result => {
-        if (result.artistName == artist && result.trackName == audio) {
-          found == true
-          // $('#sampleAudio').append(`<source src=${result.previewUrl} type="audio/x-m4a">`)
-          var obj = {
-            artist: artist,
-            song: audio,
-            previewUrl: result.previewUrl
+      var result = json.results 
+      // console.log(result);
+      $('.selected').empty();
+      $('.selected').append(`<img src=${result[0].artworkUrl100}/>`)
+      var found = false;
+      for (i = 0; i < json.results.length; i++){
+        var newTrackName = editName(result[i].trackName.toLowerCase());
+        var newArtistName = editName(result[i].artistName.toLowerCase());
+        //  console.log('edited search results: '+ newTrackName + " / " + newArtistName)
+        if (newArtistName=== artist1.toLowerCase() && newTrackName === audio1.toLowerCase()) {
+          found = true;
+          playingAudio = (result[i].previewUrl);
+          // console.log('song we are returning: ' + playingAudio);
+          play(playingAudio);
+          $('.artistName').text(`Artist: ${artist}`);
+          $('.songName').text(`Song: ${audio}`);
+          //stops for loops
+          i = json.results.length;
           }
-          returnArray.push(obj);
-          returnArray.sort(() => Math.random() - 0.5);
+        }
+        if(found === false){
+          console.log('no matching song');
+          loadPlayer(1);
 
         }
-
-      });
-      if (!found) {
-        return ''
-      }
     },
-    error: function (xhr, status, err) { }
-  });
+    error: function (xhr, status, err) {
+      }
+     }
+  );
 
+}
+
+function play(url){
+  console.log('loading: '+ url)
+  var audio = (document.querySelector('audio'));
+  audio.removeAttribute('src');
+  audio.setAttribute('src',url);
+  audio.load();
+  console.log('playing: '+ url);
+  audio.play();
+}
+//removes the featuring part of artist title because conflicting between how apis name artists;
+function editName(str){
+    //console.log('passing ' + str)
+    var res = str.split('feat').join(',').split('(').join(',').split('.').join(',').split(',');
+    res = res[0]
+    res = res.trim();
+    // console.log('returning ' + res);
+    return res;
+}
+
+function returnFalse(){
+  return false;
 }
